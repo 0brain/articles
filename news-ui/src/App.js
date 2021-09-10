800d823bfb217388767f9c7039ea6edf5c6844a0
nazar <nazar.khort@gmail.com>
2021-09-10 22:56:21 +0300

    Changed function App() and connected bootstrap.

--- news-ui/src/App.js
+++ news-ui/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+    const [articles, setArticles] = useState([])
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: 'http://127.0.0.1:8000/articles/'
+        }).then(response => {
+            setArticles(response.data)
+        })
+    }, [])
+
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h3>Список статей</h3>
+      <ul class="list-group">
+          {
+          articles.map(p => (
+              <li class="list-group-item" key={p.id}>{p.title}</li>))
+          }
+      </ul>
     </div>
   );
 }
