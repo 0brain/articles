dd5f12a82533e6dbb19541a21bd40a4396b53da6
taras <taras-bass@users.noreply.github.com>
2021-09-12 17:38:38 +0300

    Refactoring

--- news-ui/src/components/ArticleDetails.js
+++ news-ui/src/components/ArticleDetails.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import {useEffect, useState} from 'react';
-import{ useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 function ArticleDetails() {
   const [article, setArticle] = useState({})
   const [comments, setComments] = useState([])
-  let { articleId } = useParams();
+  let {articleId} = useParams();
 
   useEffect(() => {
     axios({
@@ -16,7 +16,7 @@ function ArticleDetails() {
     })
   }, [articleId])
 
-    useEffect(() => {
+  useEffect(() => {
     axios({
       method: 'GET',
       url: `http://localhost:8086/articles/${articleId}/comments/`
@@ -26,28 +26,41 @@ function ArticleDetails() {
   }, [])
 
   return (
-      <div class="container">
-          <div class="row">
-               <div class="col">
-                    <h3>{article.title}</h3>
-                    {article.content}<br></br>
-                    <br></br>
-                    {article.vote}<br></br>
-                    <br></br>
-                    Iм’я автора: {article.author_name}<br></br>
-                    Дата публікації: {article.created_at}
-                    <ul className="list">
-                             {
-                               comments.map(c => (
-                                 <li class="list-group-item">
-                                     {c.author_name}
-                                     {c.content}
-                                     {c.created_at}
-                                 </li>
-                               ))
-                             }
-                    </ul>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h3>{article.title}</h3>
+          </div>
+          <div className="row">
+            <div className="col">
+              {article.content}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              Iм’я автора: {article.author_name}
+            </div>
+            <div className="col">
+              Дата публікації: {article.created_at}
+            </div>
+          </div>
+        </div>
 
+        <div className="row justify-content-start">
+          <div className="col">
+            <div className="page-header">
+              <h1>Comments</h1>
+            </div>
+            <div className="comments-list">
+              {comments.map(c => (
+                  <div className="media">
+                    <p className="pull-right"><small>{c.created_at}</small></p>
+                    <div className="media-body">
+                      <h4 className="media-heading user_name">{c.author_name}</h4>
+                      {c.content}
+                    </div>
+                  </div>))}
+            </div>
           </div>
         </div>
       </div>
